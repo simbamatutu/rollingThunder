d61cfa878c929ccdd663c42b449945f62ed02b0a
Simba Matutu <simbamatutu@gmail.com>
2020-08-13 02:06:32 +0800

    fnaa

--- echo/app.js
+++ echo/app.js
@@ -1,15 +1,19 @@
 
 var btn = document.getElementById("btn");
-var getItem = document.getElementById("inputText");
 var res1 = document.querySelectorAll(".item");
+var itemList=document.getElementById("list-container");
+ 
 
 btn.addEventListener('click', function (e) {
-
+ 
   e.preventDefault();
-  var i = 0;
-  while(i<res1.length){
-    res1[i].innerText = getItem.value;
-    i++;
-  }
-
+  var newItem = document.getElementById("inputText").value;
+  console.log(newItem);
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'item';
+  var liText=document.createTextNode(newItem);
+  li.appendChild(liText);
+  itemList.appendChild(li);
 });
