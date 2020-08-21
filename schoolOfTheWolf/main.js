2d30f6af710b21cc6f90e1e3621450fdc11cdb18
Simba Matutu <simbamatutu@gmail.com>
2020-08-21 19:10:30 +0800

    init final

--- schoolOfTheWolf/main.js
+++ schoolOfTheWolf/main.js
@@ -1,6 +1,13 @@
 var closeOne= document.getElementById("close");
 var menu= document.getElementById('menuActive');
 var toggleMenu=document.getElementById('toggleMenu');
+var locationList=document.getElementById('locationList');
+var gearList=document.getElementById('gearList');
+var alumniList=document.getElementById('alumniList');
+var contractsList=document.getElementById('contractsList');
+
+
+
 
 closeOne.addEventListener('click', function(){
     menu.classList.toggle("menuActive");
@@ -10,4 +17,24 @@ closeOne.addEventListener('click', function(){
 toggleMenu.addEventListener('click', function(){
     menu.classList.toggle("menuActive");
     menu.style.zIndex='20';
+});
+
+locationList.addEventListener('click' , function(){
+    menu.classList.toggle("menuActive");
+    menu.style.zIndex='0';
+});
+
+gearList.addEventListener('click' , function(){
+    menu.classList.toggle("menuActive");
+    menu.style.zIndex='0';
+});
+
+alumniList.addEventListener('click' , function(){
+    menu.classList.toggle("menuActive");
+    menu.style.zIndex='0';
+});
+
+contractsList.addEventListener('click' , function(){
+    menu.classList.toggle("menuActive");
+    menu.style.zIndex='0';
 });
