482d046c0bf6203b7c3101257a3eea4c3fca99d0
Simba Matutu <simbamatutu@gmail.com>
2020-08-27 05:54:15 +0800

    0933

--- nightAndDay/js/app.js
+++ nightAndDay/js/app.js
@@ -1,9 +1,8 @@
-var time = document.getElementById('time'),
+const time = document.getElementById('time'),
   greeting = document.getElementById('greeting'),
   name = document.getElementById('name'),
   plan = document.getElementById('theplan');
 
-console.log(new Date());
 
 function setTime() {
   let today = new Date(),
@@ -23,28 +22,80 @@ function zeropad(n) {
 function greetings() {
   let today = new Date(),
     hours = today.getHours();
-
-
+     
   if (hours < 12) {
     greeting.textContent = 'Good Morning';
-    document.body.style.backgroundImage = "url('./img/morning.jpg')";
-    document.body.style.backgroundPosition = "centre";
-    document.body.style.color = "whitesmoke";
   } else if (hours < 18) {
     greeting.textContent = 'Good Afternoon';
-    document.body.style.backgroundImage = "url('./img/afternoon.jpg')";
-    document.body.style.backgroundPosition = "centre";
-    document.body.style.color = "whitesmoke";
   } else {
     greeting.textContent = 'Good Evening';
-    document.body.style.backgroundImage = "url('./img/night.jpg')";
-
-    document.body.style.backgroundSize = "cover";
-    document.body.style.color = "white";
   }
 
 }
+ 
+function setBg(){
+    let today = new Date(),
+    hours = today.getHours();
+    if(hours < 5){
+        document.body.style.backgroundImage = "url('./img/night2.jpg')";
+        document.body.style.color="white";
+    }else if(hours>= 6 && hours <= 11){
+        document.body.style.backgroundImage = "url('./img/sunrise.jpg')";
+        document.body.style.color="white"; 
+    }else if((hours>= 12 && hours <= 17) ){
+        document.body.style.backgroundImage = "url('./img/day.jpg')";
+        document.body.style.color="white"; 
+    }else if(hours == 18){
+        document.body.style.backgroundImage = "url('./img/sunset.jpg')"; 
+        document.body.style.color="white";
+    }else{
+        document.body.style.backgroundImage = "url('./img/night1.jpg')"; 
+    }
+}
+function getName(){
+
+    if(localStorage.getItem('name') === null){
+        name.textContent ='Enter name..';
+    }else{
+        name.textContent= localStorage.getItem('name');
+    }
+}
+
+function setName(e){
+    if(e.type === 'keypress'){
+        if(e.which==13 || e.keyCode == 13){
+            localStorage.setItem('name', e.target.innerHTML);
+            name.blur();
+        }
+    }else{
+        localStorage.setItem('name', e.target.innerHTML);
+    }
+}
+name.addEventListener('keypress', setName);
 
 
-setTime();
+function getPlan(){
+    if(localStorage.getItem('plan') === null){
+        name.textContent ='Enter the plan..';
+    }else{
+        plan.textContent= localStorage.getItem('plan');
+    }
+}
+
+function setPlan(e){
+    if(e.type === 'keypress'){
+        if(e.which==13 || e.keyCode == 13){
+            localStorage.setItem('plan', e.target.innerHTML);
+            plan.blur();
+        }
+    }else{
+        localStorage.setItem('plan', e.target.innerHTML);
+    }
+}
+plan.addEventListener('keypress', setPlan);
+
 greetings();
+getName();
+getPlan();
+setTime();
+setBg();
