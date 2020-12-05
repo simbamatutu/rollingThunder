a2ad2aa0c6a3f69d813028f63abf6a370053c974
simbamatutu <34451163+simbamatutu@users.noreply.github.com>
2020-12-05 22:10:50 +0800

    tomarch

--- randomquotesmachine/src/components/Navigate.js
+++ randomquotesmachine/src/components/Navigate.js
@@ -3,12 +3,12 @@ import React from 'react'
 function Navigate() {
     return (
         <div className='Navigate'>
-           <ul>
-               <li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
-               <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
-               <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            <ul>
+                <li><a href="#"><i className="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i className="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
 
-           </ul>
+            </ul>
         </div>
     )
 }
