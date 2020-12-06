98129a3ce2372a4833be0feeca8c8ecf0b6b27e6
simbamatutu <34451163+simbamatutu@users.noreply.github.com>
2020-12-06 08:08:31 +0800

    121

--- randomquotesmachine/src/components/Quotebox.js
+++ randomquotesmachine/src/components/Quotebox.js
@@ -1,44 +1,46 @@
 import React, { Component } from 'react'
 import Quote from './Quote'
 import Navigate from './Navigate'
- 
+
 export class Quotebox extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            randomText:'He who puts on the shoe knows best where is pitches!',
-            randomAuthor:'Ian D. Smith'
-        }      
-        
+            randomText: 'He who puts on the shoe knows best where is pitches!',
+            randomAuthor: 'Ian D. Smith'
+        }
+
     }
 
-   onClickHandle = ()=>{
-        var items=this.props.choosenQoute;
-            var rando= Math.floor(Math.random()*(items.length));
-             if(items[rando].text == null || items[rando].author == null){
-          rando=583;
+    onClickHandle = () => {
+        var items = this.props.choosenQoute;
+        var rando = Math.floor(Math.random() * (items.length));
+        if (items[rando].text == null || items[rando].author == null) {
+            rando = 583;
         }
-      
-                this.setState({
-                    randomText:items[rando].text,
-                    randomAuthor:items[rando].author
-       });
-        
+
+        this.setState({
+            randomText: items[rando].text,
+            randomAuthor: items[rando].author
+        });
+
     }
-    componentDidMount(){
-      /*  var items=this.props.choosenQoute;
-        var rando= Math.floor(Math.random()*(items.length));
-   
-      
-            this.setState({
-                randomText:items[rando].text,
-                randomAuthor:items[rando].author
-            })
-            */
-       
+    componentDidMount() {
+        var items = this.props.choosenQoute;
+        var rando = Math.floor(Math.random() * (items.length));
+        if (items[rando].text == null || items[rando].author == null) {
+            rando = 583;
+        }
+
+        this.setState({
+            randomText: items[rando].text,
+            randomAuthor: items[rando].author
+        });
+
+
     }
-         
- 
+
+
 
     render() {
         return (
@@ -46,7 +48,7 @@ export class Quotebox extends Component {
                 <Quote qoute={this.state.randomText} author={this.state.randomAuthor} />
                 <Navigate onClickHandle={this.onClickHandle} />
             </div>
-            
+
         )
     }
 }
